6fb8f8d7765ee77c419c93ca829055dc95cd2c2e
Joris <jote0002@stud.kea.dk>
2026-02-05 11:40:21 +0100

    Added base css variables

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,16 @@
 import "../css/globals.css";
+import { Poppins, Inter } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: '700',
+  subsets: ['latin'],
+  variable: '--font-poppins',
+})
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 
 export const metadata = {
@@ -8,9 +20,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} ${inter.variable}`}>
+
       <body>
+        <main className="max-w-6xl h-248 mx-auto mt-32 border-amber-300 border-x-2">
         {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>Hello world</h1></main>
+    <section>
+      <h1>Building Web</h1>
+      <h1>Solutions</h1>
+      <p>Building Web Solutions</p>
+      <p>Building Web Solutions</p>
+      <p>Building Web Solutions</p>
+    </section>
   );
 }
