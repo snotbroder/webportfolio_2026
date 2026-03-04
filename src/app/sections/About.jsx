5ddc4b8844fa36eda4231aaa1a071bf63bfb3084
Joris <jote0002@stud.kea.dk>
2026-03-04 13:51:19 +0100

    Updated content and image in about me

--- src/app/sections/About.jsx
+++ src/app/sections/About.jsx
@@ -11,6 +11,15 @@ export default function About() {
           About <span className="text-(--c-primary)">me</span>
         </h2>
       </header>
+      <section className="flex flex-col gap-6 place-items-center lg:flex-row lg:gap-20 lg:place-items-start">
+        <p>
+          I’m a full-stack development student with <strong>over two years of experience</strong> building reliable, well-structured, and user-focused web applications, with a strong focus on usability, performance, and clean design. Originally from the Netherlands, I’ve lived in Denmark since childhood and now call Vesterbro, Copenhagen home, where I live with my partner and our dog. I speak <strong>Danish, English, and Dutch</strong>, have experience with <strong>German and Spanish</strong>, and enjoy working in international environments. Outside of work, I value time with friends, exploring the city, and traveling across Europe.
+        </p>
+        <div className="min-w-48 lg:min-w-64 silver-img-container">
+          <Image className="silver-img" width={400} height={400} src="/silver.png" alt="Eurasier dog standing to the side, looking towards the camera"></Image>
+          <p className="small mt-2">My sweet dog Silver</p>
+        </div>
+      </section>
       <section className="group flex flex-col gap-8 bg-(--c-secondary5) rounded-md border border-(--c-secondary10) overflow-hidden md:flex-row">
         <div className="flex flex-col gap-8 p-4 md:p-6 lg:p-10">
           <p>
